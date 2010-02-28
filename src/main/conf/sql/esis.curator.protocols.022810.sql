8fa2ed24bd967da848fb24ba8b082a52c33a957f
Chris Mattmann <chris.mattmann@gmail.com>
2010-02-28 17:26:20 +0000

    - progress towards CA-517 Curate PLCO data

---
+++ src/main/conf/sql/esis.curator.protocols.022810.sql
@@ -0,0 +1,11 @@
+-- Copyright 2010 California Institute of Technology. ALL RIGHTS
+-- RESERVED. U.S. Government Sponsorship acknowledged.
+--
+-- $Id$
+--
+-- Author: Chris A. Mattmann
+-- 
+-- I had to add these protocols manually during eCAS curation for the
+-- March 2010 SC meeting in Tempe, AZ
+
+INSERT INTO protocols (ID, Name, StudyDesign, Abstract) VALUES (312, 'PLCO Ovarian Validation Study', 'Case/control', '');
