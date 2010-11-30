d9424081cb95e038f0ef56f1d6c43684cd703b8a
Chris Mattmann <chris.mattmann@gmail.com>
2010-11-30 14:45:30 +0000

    - revert changes from r7783:   - Revert system.out.println messages at the end   - Met file cleanup no longer happens and it's only a copy - should be a move as specified   - remove the if(false) clause
      - NOTE: the commons-httpclient library handles redirects (see here: http://s.apache.org/PNU) we shouldn't need to develop this custom code.

--- src/main/java/gov/nasa/jpl/edrn/ecas/tools/ExtractAndDownloadGLSUrls.java
+++ src/main/java/gov/nasa/jpl/edrn/ecas/tools/ExtractAndDownloadGLSUrls.java
@@ -12,7 +12,6 @@ import gov.nasa.jpl.oodt.cas.metadata.util.PathUtils;
 //JDK imports
 import java.io.File;
 import java.io.FileInputStream;
-import java.net.URLEncoder;
 
 //APACHE imports
 import org.apache.commons.httpclient.Credentials;
@@ -22,8 +21,6 @@ import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.Header;
-
 import org.apache.commons.io.FileUtils;
 
 /**
@@ -35,22 +32,20 @@ import org.apache.commons.io.FileUtils;
  * @version $Revision$
  * 
  */
-
 public class ExtractAndDownloadGLSUrls {
 
   public static void main(String[] args) throws Exception {
     // first param is abs dir path
     String usage = "java " + ExtractAndDownloadGLSUrls.class.getName()
         + " <met dir> <user> <pass>\n";
+    String glsMetDir = args[0];
+    String glsUsername = args[1];
+    String glsPassword = args[2];
 
     if (args.length != 3) {
       System.err.println(usage);
       System.exit(1);
     }
-
-    String glsMetDir = args[0];
-    String glsUsername = args[1];
-    String glsPassword = args[2];
 
     if (!glsMetDir.endsWith("/"))
       glsMetDir += "/";
@@ -61,97 +56,43 @@ public class ExtractAndDownloadGLSUrls {
       // compute target path
       String targetPathSpec = "/[FileLocation]/[subjectId]/[Filename]";
       String targetPathMetSpec = "/[FileLocation]/[subjectId]/[Filename].met";
-
-      if (false) {
-	  targetPathSpec = "/home/jtran/data-stage/[subjectId]/[Filename]";
-	  targetPathMetSpec = "/home/jtran/data-stage/[subjectId]/[Filename].met";
-      }
-
       String targetPath = PathUtils.replaceEnvVariables(targetPathSpec, met);
       String targetMetPath = PathUtils.replaceEnvVariables(targetPathMetSpec, met);
       String glsMetUrl = met.getMetadata("ProductLocation");
-
-      // make sure that " " is not allowed in URL
-      glsMetUrl = glsMetUrl.replaceAll(" ", "%20");
-
-      System.out.println("Downloading [" + glsMetUrl + "] to file path: [" + targetPath + "]");
+      System.out.println("Downloading [" + glsMetUrl + "] to file path: ["
+          + targetPath + "]");
       new File(targetPath).getParentFile().mkdirs();
 
       HttpClient httpClient = new HttpClient();
+      GetMethod get = new GetMethod(glsMetUrl);
+      String response = null;
+      Credentials defaultcreds = new UsernamePasswordCredentials(glsUsername,
+          glsPassword);     
+      httpClient.getState().setCredentials(new AuthScope(AuthScope.ANY_HOST, 443, AuthScope.ANY_REALM), defaultcreds);
 
+      get.setDoAuthentication(true);
       boolean downloadSuccess = false;
-      String redirectLocation = "";
-
-      {
-	  GetMethod get = new GetMethod(glsMetUrl);
-	  String response = null;
-	  Credentials defaultcreds = 
-             new UsernamePasswordCredentials(glsUsername, glsPassword);     
-	  httpClient.getState().setCredentials(new AuthScope(AuthScope.ANY_HOST, 443, AuthScope.ANY_REALM), defaultcreds);
-	  
-	  get.setDoAuthentication(true);
-	  
-	  try {
-	      httpClient.executeMethod(get);
-	      if (get.getStatusCode() == 302 ) {
-		  Header locationHeader = get.getResponseHeader("location");
-		  if (locationHeader != null) {
-		      redirectLocation = locationHeader.getValue();
-		  } else {
-		      throw new HttpException("ERROR: Unknown URL redirection address");
-		  }
-	      }
-	      else if (get.getStatusCode() != HttpStatus.SC_OK) {
-		  // throw new HttpException(get.getStatusLine().toString());
-	      }
-	      response = get.getResponseBodyAsString().trim();
-	      FileUtils.writeStringToFile(new File(targetPath), response, "UTF-8");
-	      downloadSuccess = true;
-	  } catch (Exception e) {
-	      e.printStackTrace();
-	  } finally {
-	      get.releaseConnection();
-	  }
-      }
-
-      // in case the file has been redirected try again
-      if (! redirectLocation.equals("")) {
-	  // redirectLocation = redirectLocation.replace(' ', '+');	
-	  GetMethod get = new GetMethod(redirectLocation.replaceAll(" ", "%20"));
-	  String response = null;
-
-	  System.out.println("New location: [" + redirectLocation + "]");
-	  Credentials defaultcreds = 
-             new UsernamePasswordCredentials(glsUsername, glsPassword);     
-	  httpClient.getState().setCredentials(new AuthScope(AuthScope.ANY_HOST, 443, AuthScope.ANY_REALM), defaultcreds);
-	  
-	  get.setDoAuthentication(true);
-	  
-	  try {
-	      httpClient.executeMethod(get);
-	      if (get.getStatusCode() != HttpStatus.SC_OK) {
-		  throw new HttpException(get.getStatusLine().toString());
-	      }
-	      response = get.getResponseBodyAsString().trim();
-	      FileUtils.writeStringToFile(new File(targetPath), response, "UTF-8");
-	      downloadSuccess = true;
-	  } catch (Exception e) {
-	      e.printStackTrace();
-	  } finally {
-	      get.releaseConnection();
-	  }
+      try {
+        httpClient.executeMethod(get);
+        if (get.getStatusCode() != HttpStatus.SC_OK) {
+          throw new HttpException(get.getStatusLine().toString());
+        }
+        response = get.getResponseBodyAsString().trim();
+        FileUtils.writeStringToFile(new File(targetPath), response, "UTF-8");
+        downloadSuccess = true;
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        get.releaseConnection();
       }
 
       if (downloadSuccess) {
-        System.out.println("*** DOWNLOAD: SUCCESS ***");
-        // Copy met file
-        System.out.println("Copying met file: [" + glsMetFile.getAbsolutePath()
+        // move met file
+        System.out.println("Moving met file: [" + glsMetFile.getAbsolutePath()
             + "] to [" + targetMetPath + "]");
         FileUtils.copyFile(glsMetFile, new File(targetMetPath));
         glsMetFile.deleteOnExit();
 
-      } else {
-        System.out.println("*** DOWNLOAD: FAILED ***");
       }
     }
   }
